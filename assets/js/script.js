7895d270564c11e103c9c98241287ddc9c3ae17a
deenafe <tymbalink@hotmail.com>
2022-10-04 09:47:39 +0000

    Add basic js structure for quiz game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
 // Popup modal for "How to play" button
-let popup = document.getElementById("popup");
-let popOpen = document.getElementById("btn-open");
-let popClose = document.getElementById("btn-close");
+
+// let popup = document.getElementById("popup");
+// let popOpen = document.getElementById("btn-open");
+// let popClose = document.getElementById("btn-close");
 
 function openPopup(event) {
+    let popup = document.getElementById("popup");
+    let popOpen = document.getElementById("btn-open");
+    let popClose = document.getElementById("btn-close");
     popup.classList.add("popup-modal");
 }
+
 
 function closePopup(event) {
     popup.classList.remove("popup-modal");
@@ -14,12 +19,12 @@ function closePopup(event) {
 popOpen.addEventListener('click', openPopup);
 popClose.addEventListener('click', closePopup);
 
-// Start quiz button
-let startButton = document.getElementById("start-btn")
-let startQuiz = document.getElementsByClassName("start-quiz")
 
+// Start quiz button
 
 function displayQuestions(event) {
+    let startButton = document.getElementById("start-btn")
+    let startQuiz = document.getElementsByClassName("start-quiz")
     let displayQuestions = document.getElementById("questions-area").style.display = "block";
 }
 
@@ -30,5 +35,37 @@ function displayQuestions(event) {
 
 startButton.addEventListener('click', displayQuestions)
 // startQuiz.addEventListener('click', removeStart)
+
+// Function for submit quiz
+
+submitQuiz() {
+
+}
+
+// Check correct answers
+
+checkAnswer() {
+
+}
+
+// Display answers checked by user
+
+displayCheckedAnswer() {
+
+}
+
+// Tally the score 
+
+incrementScore() {
+
+}
+
+
+// To add an icon to the wrong/correct answers 
+
+markQuestions() {
+
+}
+
 
 

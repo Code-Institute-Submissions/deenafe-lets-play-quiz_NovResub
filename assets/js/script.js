608eb84432483271bb2272cb86a2e6f6b2cbf6d4
deenafe <tymbalink@hotmail.com>
2022-10-07 15:22:42 +0000

    Add validate function to answer all questions before submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+
 function modalOpen() {
     popup.classList.add("popup-modal"); 
 }
@@ -15,8 +17,13 @@ popClose.addEventListener('click', modalClose);
 
 //  This displays the questions after user cilck the "Start Quiz" button
 
+// const startClose = document.getElementsByClassName('start-quiz')
+
 function displayQuestions() {
     document.getElementById("questions-area").style.display = "block";
+    // document.getElementsByClassName('start-quiz').style.display = "block";
+    
+
 }
 
 let startButton = document.getElementById("start-btn");
@@ -43,6 +50,7 @@ function displayRadioValue() {
         if(question1[i].checked) {
             document.getElementById("result1").innerHTML
                         = "You selected: " + question1[i].value;
+                        document.getElementsByClassName('question-field')[0].style.border = "1px solid black"
         }
     }
 
@@ -50,6 +58,7 @@ function displayRadioValue() {
         if(question2[i].checked) {
             document.getElementById("result2").innerHTML
                         = "You selected: " + question2[i].value;
+                        document.getElementsByClassName('question-field')[1].style.border = "1px solid black"
         }
     }
 
@@ -57,6 +66,7 @@ function displayRadioValue() {
         if(question3[i].checked) {
             document.getElementById("result3").innerHTML
                         = "You selected: " + question3[i].value;
+                        document.getElementsByClassName('question-field')[2].style.border = "1px solid black"
         }
     }
 
@@ -64,6 +74,7 @@ function displayRadioValue() {
         if(question4[i].checked) {
             document.getElementById("result4").innerHTML
                         = "You selected: " + question4[i].value;
+                        document.getElementsByClassName('question-field')[3].style.border = "1px solid black"
         }
     }
 
@@ -71,6 +82,7 @@ function displayRadioValue() {
         if(question5[i].checked) {
             document.getElementById("result5").innerHTML
                         = "You selected: " + question5[i].value;
+                        document.getElementsByClassName('question-field')[4].style.border = "1px solid black"
         }
     }
 
@@ -78,6 +90,7 @@ function displayRadioValue() {
         if(question6[i].checked) {
             document.getElementById("result6").innerHTML
                         = "You selected: " + question6[i].value;
+                        document.getElementsByClassName('question-field')[5].style.border = "1px solid black"
         }
     }
 
@@ -85,6 +98,7 @@ function displayRadioValue() {
         if(question7[i].checked) {
             document.getElementById("result7").innerHTML
                         = "You selected: " + question7[i].value;
+                        document.getElementsByClassName('question-field')[6].style.border = "1px solid black"
         }
     }
 
@@ -92,6 +106,7 @@ function displayRadioValue() {
         if(question8[i].checked) {
             document.getElementById("result8").innerHTML
                         = "You selected: " + question8[i].value;
+                        document.getElementsByClassName('question-field')[7].style.border = "1px solid black"
         }
     }
 
@@ -99,6 +114,7 @@ function displayRadioValue() {
         if(question9[i].checked) {
             document.getElementById("result9").innerHTML
                         = "You selected: " + question9[i].value;
+                        document.getElementsByClassName('question-field')[8].style.border = "1px solid black"
         }
     }
 
@@ -106,6 +122,7 @@ function displayRadioValue() {
         if(question10[i].checked) {
             document.getElementById("result10").innerHTML
                         = "You selected: " + question10[i].value;
+                        document.getElementsByClassName('question-field')[9].style.border = "1px solid black"
         }
     }
     
@@ -182,27 +199,160 @@ submitButton.addEventListener('click', checkAnswer);
 let gradeMessage = document.getElementById('grade-message');
 
 function scoreGrading() {
-    let scoreArea = document.getElementById('score-area')
-    let pics = ["../images/poor1.webp", "../images/better2.webp", "../images/dobetter3.webp", "../images/fantastic4.webp", "../images/genius5.webp"]
+    let pics = ["assets/images/poor1.webp", "assets/images/better2.webp", "assets/images/dobetter3.webp", "assets/images/fantastic4.webp", "assets/images/genius5.webp"]
    
 
     if (correct < 1) {
-        gradeMessage.innerHTML = `<img src="${pics[1]}" alt="Two men with gloomy demeanor">  <span>A poor result, why not try again</span> `}
+        gradeMessage.innerHTML = `<img src="${pics[0]}" alt="Two men with gloomy demeanor"> <br> <span>A poor result, why not try again</span> `}
      
      if (correct > 1 && correct < 5) {
-        gradeMessage.innerHTML = `<img src="${pics[2]}" alt="Man in kung-fu pose challenging viewer to do better">  <span> You can do better, why not try again. </span> ` }
+        gradeMessage.innerHTML = `<img src="${pics[1]}" alt="Man in kung-fu pose challenging viewer to do better"> <br> <span> You can do better, why not try again. </span> ` }
      
      if (correct >= 5 && correct < 8) {
-        gradeMessage.innerHTML = `<img src="${pics[3]}" alt="Man encouraging and urging viewer to improve"> <span> You did good, try harder next time. </span>` }
+        gradeMessage.innerHTML = `<img src="${pics[2]}" alt="Man encouraging and urging viewer to improve"> <br> <span> You did good, but you can do better. </span>` }
      
      if (correct >= 8 && correct < 10) {
-        gradeMessage.innerHTML = `<img src="${pics[4]}" alt="Man performing on stage in exicted mood"> <span> You did good, try harder next time. </span>`}
+        gradeMessage.innerHTML = `<img src="${pics[3]}" alt="Man performing on stage in exicted mood"> <br> <span> You did Great!!!. </span>`}
      
      if (correct == 10) {
-        gradeMessage.innerHTML = `<img src="${pics[5]}" alt="A famous genius, Albert Einstein"> <span> Excellent, you must be a genuis. </span>`}
+        gradeMessage.innerHTML = `<img src="${pics[4]}" alt="A famous genius, Albert Einstein"> <br> <span> Excellent!!! you must be a genuis. </span>`}
 
 }
 
 submitButton.addEventListener('click', scoreGrading)
 
-// let allAnswered = [question1[i].checked, question2.checked[i], question3.checked[i], question4.checked[i], question5.checked[i], question6.checked[i], question7.checked[i], question8.checked[i], question9.checked[i], question10.checked[i] ]
+// Validate form to ensure all questions are answered
+
+const quest1 = document.getElementsByClassName('question1')
+const quest2 = document.getElementsByClassName('question2')
+const quest3 = document.getElementsByClassName('question3')
+const quest4 = document.getElementsByClassName('question4')
+const quest5 = document.getElementsByClassName('question5')
+const quest6 = document.getElementsByClassName('question6')
+const quest7 = document.getElementsByClassName('question7')
+const quest8 = document.getElementsByClassName('question8')
+const quest9 = document.getElementsByClassName('question9')
+const quest10 = document.getElementsByClassName('question10')
+
+function validate(event) {
+    
+    for ( let i = 0; i < quest1.length; i++) {
+        if (quest1[i].checked) {
+            return true;
+        } else {
+            document.getElementById("error").innerHTML= "Please attempt to answer all questions";
+            document.getElementsByClassName('question-field')[0].style.border = "2px solid red"
+            document.getElementById('score-area').style.visibility = "hidden";
+            event.preventDefault();
+        }
+    }
+
+    for ( let i = 0; i < quest2.length; i++) {
+        if (quest2[i].checked) {
+            return true;
+        } else {
+            document.getElementById("error").innerHTML= "Please attempt to answer all questions";
+            document.getElementsByClassName('question-field')[1].style.border = "2px solid red"
+            document.getElementById('score-area').style.visibility = "hidden";
+            event.preventDefault();
+        }
+    }
+
+    for ( let i = 0; i < quest3.length; i++) {
+        if (quest3[i].checked) {
+            return true;
+        } else {
+            document.getElementById("error").innerHTML= "Please attempt to answer all questions";
+            document.getElementsByClassName('question-field')[2].style.border = "2px solid red"
+            document.getElementById('score-area').style.visibility = "hidden";
+            event.preventDefault();
+        }
+    }
+
+    for ( let i = 0; i < quest4.length; i++) {
+        if (quest4[i].checked) {
+            return true;
+        } else {
+            document.getElementById("error").innerHTML= "Please attempt to answer all questions";
+            document.getElementsByClassName('question-field')[3].style.border = "2px solid red"
+            document.getElementById('score-area').style.visibility = "hidden";
+            event.preventDefault();
+        }
+    }
+
+    for ( let i = 0; i < quest5.length; i++) {
+        if (quest5[i].checked) {
+            return true;
+        } else {
+            document.getElementById("error").innerHTML= "Please attempt to answer all questions";
+            document.getElementsByClassName('question-field')[4].style.border = "2px solid red"
+            document.getElementById('score-area').style.visibility = "hidden";
+            event.preventDefault();
+              
+        }
+    }
+
+    for ( let i = 0; i < quest6.length; i++) {
+        if (quest6[i].checked) {
+            return true;
+        } else {
+            document.getElementById("error").innerHTML= "Please attempt to answer all questions";
+            document.getElementsByClassName('question-field')[5].style.border = "2px solid red"
+            document.getElementById('score-area').style.visibility = "hidden";
+            event.preventDefault();
+             
+        }
+    }
+
+    for ( let i = 0; i < quest7.length; i++) {
+        if (quest7[i].checked) {
+            return true;
+        } else {
+            document.getElementById("error").innerHTML= "Please attempt to answer all questions";
+            document.getElementsByClassName('question-field')[6].style.border = "2px solid red"
+            document.getElementById('score-area').style.visibility = "hidden";
+            event.preventDefault();
+             
+        }
+    }
+
+    for ( let i = 0; i < quest8.length; i++) {
+        if (quest8[i].checked) {
+            return true;
+        } else {
+            document.getElementById("error").innerHTML= "Please attempt to answer all questions";
+            document.getElementsByClassName('question-field')[7].style.border = "2px solid red"
+            document.getElementById('score-area').style.visibility = "hidden";
+            event.preventDefault();
+              
+        }
+    }
+
+    for ( let i = 0; i < quest9.length; i++) {
+        if (quest9[i].checked) {
+            return true;
+        } else {
+            document.getElementById("error").innerHTML= "Please attempt to answer all questions";
+            document.getElementsByClassName('question-field')[8].style.border = "2px solid red"
+            document.getElementById('score-area').style.visibility = "hidden";
+            event.preventDefault();
+                
+        }
+    }
+
+    for ( let i = 0; i < quest10.length; i++) {
+        if (quest10[i].checked) {
+            return true;
+        } else {
+            document.getElementById("error").innerHTML= "Please attempt to answer all questions";
+            document.getElementsByClassName('question-field')[9].style.border = "2px solid red"
+            document.getElementById('score-area').style.visibility = "hidden";
+            event.preventDefault();
+                
+        }
+    }
+
+} 
+
+const form = document.getElementById('quiz-form');
+form.addEventListener('submit', validate)

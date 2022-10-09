1e3e45f63b40bb34735d44b76c7b5c48a0bd4ea1
deenafe <tymbalink@hotmail.com>
2022-10-09 14:56:44 +0000

    Add comments to code and clear coomented out code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+/** Function for popup modal on Let's Play button */
+
 function modalOpen() {
     popup.classList.add("popup-modal"); 
 }
@@ -9,9 +11,10 @@ let popClose = document.getElementsByTagName("button")[1];
 popOpen.addEventListener('click', modalOpen);
 popClose.addEventListener('click', modalClose);
 
-//  This displays the questions after user cilck the "Start Quiz" button
 
-// const startClose = document.getElementsByClassName('start-quiz')
+/** This function will display the questions when "Start Quiz" button is clicked
+ * It also remove the "Start Quiz" container after questions display
+  */
 
 let startRemove = document.getElementById('start-quiz')
 
@@ -23,7 +26,7 @@ function displayQuestions() {
 let startButton = document.getElementById("start-btn");
 startButton.addEventListener('click', displayQuestions);
 
-// Displays Seclected Radio values
+// Variables for each radio group
 const question1 = document.getElementsByName('question1');
 const question2 = document.getElementsByName('question2');
 const question3 = document.getElementsByName('question3');
@@ -35,8 +38,12 @@ const question8 = document.getElementsByName('question8');
 const question9 = document.getElementsByName('question9');
 const question10 = document.getElementsByName('question10');
 
+/** This function loops through each radio group to get the selected input
+ * It displays the selected input below the questions
+ * It changes thickness of the fieldset border when radio is checked
+ */
 function displayRadioValue() {
-    for (i = 0; i < question1.length; i++) {
+    for (let i = 0; i < question1.length; i++) {
         if(question1[i].checked) {
             document.getElementById("result1").innerHTML
                         = "You selected: " + question1[i].value;
@@ -44,7 +51,7 @@ function displayRadioValue() {
         }
     }
 
-    for (i = 0; i < question2.length; i++) {
+    for (let i = 0; i < question2.length; i++) {
         if(question2[i].checked) {
             document.getElementById("result2").innerHTML
                         = "You selected: " + question2[i].value;
@@ -52,7 +59,7 @@ function displayRadioValue() {
         }
     }
 
-    for (i = 0; i < question3.length; i++) {
+    for (let i = 0; i < question3.length; i++) {
         if(question3[i].checked) {
             document.getElementById("result3").innerHTML
                         = "You selected: " + question3[i].value;
@@ -60,7 +67,7 @@ function displayRadioValue() {
         }
     }
 
-    for (i = 0; i < question4.length; i++) {
+    for (let i = 0; i < question4.length; i++) {
         if(question4[i].checked) {
             document.getElementById("result4").innerHTML
                         = "You selected: " + question4[i].value;
@@ -68,7 +75,7 @@ function displayRadioValue() {
         }
     }
 
-    for (i = 0; i < question5.length; i++) {
+    for (let i = 0; i < question5.length; i++) {
         if(question5[i].checked) {
             document.getElementById("result5").innerHTML
                         = "You selected: " + question5[i].value;
@@ -76,7 +83,7 @@ function displayRadioValue() {
         }
     }
 
-    for (i = 0; i < question6.length; i++) {
+    for (let i = 0; i < question6.length; i++) {
         if(question6[i].checked) {
             document.getElementById("result6").innerHTML
                         = "You selected: " + question6[i].value;
@@ -84,7 +91,7 @@ function displayRadioValue() {
         }
     }
 
-    for (i = 0; i < question7.length; i++) {
+    for (let i = 0; i < question7.length; i++) {
         if(question7[i].checked) {
             document.getElementById("result7").innerHTML
                         = "You selected: " + question7[i].value;
@@ -92,7 +99,7 @@ function displayRadioValue() {
         }
     }
 
-    for (i = 0; i < question8.length; i++) {
+    for (let i = 0; i < question8.length; i++) {
         if(question8[i].checked) {
             document.getElementById("result8").innerHTML
                         = "You selected: " + question8[i].value;
@@ -100,7 +107,7 @@ function displayRadioValue() {
         }
     }
 
-    for (i = 0; i < question9.length; i++) {
+    for (let i = 0; i < question9.length; i++) {
         if(question9[i].checked) {
             document.getElementById("result9").innerHTML
                         = "You selected: " + question9[i].value;
@@ -108,7 +115,7 @@ function displayRadioValue() {
         }
     }
 
-    for (i = 0; i < question10.length; i++) {
+    for (let i = 0; i < question10.length; i++) {
         if(question10[i].checked) {
             document.getElementById("result10").innerHTML
                         = "You selected: " + question10[i].value;
@@ -130,8 +137,12 @@ question9.forEach(el => el.addEventListener('click', displayRadioValue));
 question10.forEach(el => el.addEventListener('click', displayRadioValue));
 
 
-//  This will check the correct answers and assign scores 
+
 let correct = 0;
+
+/** This function checks if the answer selecgted by user is correct
+ * It tallies the number of correct answers and displays score 
+ */
 function checkAnswer() {
     
     if (question1[1].checked === true) {
@@ -173,9 +184,11 @@ const submitButton = document.getElementById('btn-sub');
 submitButton.addEventListener('click', checkAnswer);
 
 
-// This should display a message and image depending on the score
-let gradeMessage = document.getElementById('grade-message');
 
+let gradeMessage = document.getElementById('grade-message');
+/** This function display an image and message on the "score-area" container
+ * Depending on the score (number of correct answers), a diferent image and message will display
+ */
 function scoreGrading() {
     let pics = ["assets/images/poor1.webp", "assets/images/better2.webp", "assets/images/dobetter3.webp", "assets/images/fantastic4.webp", "assets/images/genius5.webp"]
 
